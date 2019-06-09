8799c22b805b32b5cc0adae27f80da1219a1ec91
David Shah <dave@ds0.me>
2019-06-09 12:23:50 +0100

    rapidwright/json2dcp: Import some connectivity
    Signed-off-by: David Shah <dave@ds0.me>

--- rapidwright/java/json2dcp.java
+++ rapidwright/java/json2dcp.java
@@ -3,8 +3,10 @@ package dev.fpga.rapidwright;
 import com.google.gson.stream.JsonReader;
 import com.xilinx.rapidwright.design.*;
 import com.xilinx.rapidwright.device.PartNameTools;
+import com.xilinx.rapidwright.device.*;
 import com.google.gson.*;
 import com.xilinx.rapidwright.util.RapidWright;
+import jnr.ffi.annotations.In;
 import org.json.JSONObject;
 
 import java.io.FileNotFoundException;
@@ -130,7 +132,7 @@ public class json2dcp {
                     if (conn.size() > 0) {
                         NextpnrNet net = nets.get(conn.get(0).getAsInt());
                         port.net = net;
-                        if (port.type == PortDirection.PORT_IN) {
+                        if (port.type == PortDirection.PORT_OUT) {
                             net.driver = port;
                         } else {
                             net.users.add(port);
@@ -153,6 +155,10 @@ public class json2dcp {
         }
     }
 
+    public static String escape_name(String name) {
+        return name.replace("\\", "__").replace("/", "_");
+    }
+
     public static void main(String[] args) throws FileNotFoundException {
 
         if (args.length < 3) {
@@ -169,7 +175,45 @@ public class json2dcp {
         for (NextpnrCell nc : ndes.cells.values()) {
             if (!nc.attrs.containsKey("X_ORIG_TYPE"))
                 continue;
+            if (nc.type.equals("IOB_OUTBUF") || nc.type.equals("IOB_IBUFCTRL"))
+                continue;
             nc.rwCell = des.createAndPlaceCell(nc.name, Unisim.valueOf(nc.attrs.get("X_ORIG_TYPE")), nc.attrs.get("NEXTPNR_BEL"));
+        }
+
+        for (NextpnrNet nn : ndes.nets.values()) {
+            //System.out.println("create net " + nn.name);
+            Net n = des.createNet(escape_name(nn.name));
+            nn.rwNet = n;
+            if (nn.driver != null && nn.driver.cell.rwCell != null) {
+                if (!nn.driver.cell.attrs.containsKey("X_ORIG_PORT_" + nn.driver.name))
+                    continue;
+                //System.out.println("connect " + n.getName() + " <- " + nn.driver.cell.name + "." + nn.driver.name);
+                n.connect(nn.driver.cell.rwCell, nn.driver.cell.attrs.get("X_ORIG_PORT_" + nn.driver.name));
+            }
+            for (NextpnrCellPort usr : nn.users) {
+                if (usr.cell.rwCell != null) {
+                    if (!usr.cell.attrs.containsKey("X_ORIG_PORT_" + usr.name))
+                        continue;
+                    //System.out.println("connect " + n.getName() + " -> " + usr.cell.name + "." + usr.name);
+                    n.connect(usr.cell.rwCell, usr.cell.attrs.get("X_ORIG_PORT_" + usr.name));
+                }
+            }
+
+            String[] routing = nn.attrs.get("ROUTING").split(";");
+            for (int i = 0; i < routing.length; i+=3) {
+                String wire = routing[i];
+                String pip = routing[i+1];
+                if (pip.isEmpty() || pip.startsWith("SITEPIP"))
+                    continue;
+                String[] sp = pip.split("/");
+                String tile = sp[0];
+                String[] wires = sp[1].split("\\.");
+                Tile t = des.getDevice().getTile(tile);
+                int src = Integer.parseInt(wires[0]), dst = Integer.parseInt(wires[1]);
+                if (src < t.getWireCount() && dst < t.getWireCount())
+                    n.addPIP(t.getPIP(src, dst));
+            }
+
         }
 
         des.writeCheckpoint(args[2]);
