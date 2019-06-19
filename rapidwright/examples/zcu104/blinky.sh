a107e34d79c43551b00e700bdacaacd5537af2c6
David Shah <dave@ds0.me>
2019-06-19 18:50:37 +0100

    rapidwright: Fix zcu104 script
    Signed-off-by: David Shah <dave@ds0.me>

--- rapidwright/examples/zcu104/blinky.sh
+++ rapidwright/examples/zcu104/blinky.sh
@@ -4,5 +4,3 @@ yosys -p "synth_xilinx -flatten -arch xcup -nobram -top top; write_json blinky.j
 ../../../nextpnr-rapidwright --chipdb ../../xczu7ev.bin --json blinky.json --xdc zcu104.xdc --write blinky_routed.json
 java -jar ../../../rapidwright_json2dcp.jar xczu7ev-ffvc1156-2-e blinky_routed.json blinky.dcp
 vivado -mode batch -nojournal -nolog  -source blinky.tcl
-iverilog -o blinky_tb blinky_tb.v blinky_out.v -y${XILINX_VIVADO}/data/verilog/src/unisims ${XILINX_VIVADO}/data/verilog/src/glbl.v
-./blinky_tb
