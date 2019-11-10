d082bbdb885058e8d02c97a55961fec13b8d71a5
David Shah <dave@ds0.me>
2019-11-10 15:08:09 +0000

    router2: Bounding box improvements
    Signed-off-by: David Shah <dave@ds0.me>

--- xilinx/arch.cc
+++ xilinx/arch.cc
@@ -434,80 +434,40 @@ delay_t Arch::estimateDelay(WireId src, WireId dst, bool debug) const
 
 ArcBounds Arch::getRouteBoundingBox(WireId src, WireId dst) const
 {
-    // FIXME: reduce copy and paste with estimateDelay
-    int src_x, src_y, dst_x, dst_y;
-    int src_intent = wireIntent(src);
-    // if (src_intent == ID_PSEUDO_GND || dst_intent == ID_PSEUDO_VCC)
-    //    return 500;
+
     int dst_tile = dst.tile == -1 ? chip_info->nodes[dst.index].tile_wires[0].tile : dst.tile;
     int src_tile = src.tile == -1 ? chip_info->nodes[src.index].tile_wires[0].tile : src.tile;
 
+    int x0, x1, y0, y1;
+    x0 = src_tile % chip_info->width;
+    x1 = x0;
+    y0 = src_tile / chip_info->width;
+    y1 = y0;
+    auto expand = [&](int x, int y) {
+        x0 = std::min(x0, x);
+        x1 = std::max(x1, x);
+        y0 = std::min(y0, y);
+        y1 = std::max(y1, y);
+    };
+
+    expand(dst_tile % chip_info->width, dst_tile / chip_info->width);
+
     if (sink_locs.count(dst)) {
-        dst_x = sink_locs.at(dst).x;
-        dst_y = sink_locs.at(dst).y;
+        expand(sink_locs.at(dst).x, sink_locs.at(dst).y);
     } else if (dst.tile != -1 && chip_info->tile_insts[dst.tile].num_sites > 0) {
         auto &site = chip_info->tile_insts[dst.tile].site_insts[wireInfo(dst).site != -1 ? wireInfo(dst).site : 0];
         if (site.inter_x != -1) {
-            dst_x = site.inter_x;
-            dst_y = site.inter_y;
-        } else {
-            dst_x = dst.tile % chip_info->width;
-            dst_y = dst.tile / chip_info->width;
+            expand(site.inter_x, site.inter_y);
         }
-    } else {
-        dst_x = dst_tile % chip_info->width;
-        dst_y = dst_tile / chip_info->width;
     }
 
-    if (src.tile == -1) {
-        if (src_intent == ID_PSEUDO_GND || src_intent == ID_PSEUDO_VCC) {
-            if (gnd_glbl == IdString()) {
-                gnd_glbl = id("PSEUDO_GND_WIRE_GLBL");
-                gnd_row = id("PSEUDO_GND_WIRE_ROW");
-                vcc_glbl = id("PSEUDO_VCC_WIRE_GLBL");
-                vcc_row = id("PSEUDO_VCC_WIRE_ROW");
-            }
-
-            src_x = src_tile % chip_info->width;
-            src_y = src_tile / chip_info->width;
-            if (wireInfo(src).name == gnd_row.index || wireInfo(src).name == vcc_row.index)
-                src_x = chip_info->width / 2;
-        } else {
-            auto &src_n = chip_info->nodes[src.index];
-            src_x = -1;
-            src_y = -1;
-            for (int i = 0; i < std::min(200, src_n.num_tile_wires); i++) {
-                // Approximate the nearest location to dest
-                int ti = src_n.tile_wires[i].tile;
-                auto &tw = chip_info->tile_types[chip_info->tile_insts[ti].type].wire_data[src_n.tile_wires[i].index];
-                if (tw.num_downhill == 0 && src_intent != ID_NODE_PINFEED)
-                    continue;
-                int tix = ti % chip_info->width, tiy = ti / chip_info->width;
-                if (src_x == -1 || std::abs(tix - dst_x) < std::abs(src_x - dst_x))
-                    src_x = tix;
-                if (src_y == -1 || std::abs(tiy - dst_y) < std::abs(src_y - dst_y))
-                    src_y = tiy;
-            }
-            if (src_x == -1) {
-                src_x = chip_info->nodes[src.index].tile_wires[0].tile % chip_info->width;
-                src_y = chip_info->nodes[src.index].tile_wires[0].tile / chip_info->width;
-            }
-        }
-
-    } else if (src.tile != -1 && chip_info->tile_insts[src.tile].num_sites > 0) {
-        auto &site = chip_info->tile_insts[src.tile].site_insts[wireInfo(src).site != -1 ? wireInfo(src).site : 0];
+    if (src.tile != -1 && chip_info->tile_insts[src.tile].num_sites > 0) {
+        auto &site = chip_info->tile_insts[dst.tile].site_insts[wireInfo(dst).site != -1 ? wireInfo(dst).site : 0];
         if (site.inter_x != -1) {
-            src_x = site.inter_x;
-            src_y = site.inter_y;
-        } else {
-            src_x = src.tile % chip_info->width;
-            src_y = src.tile / chip_info->width;
+            expand(site.inter_x, site.inter_y);
         }
-    } else {
-        src_x = src_tile % chip_info->width;
-        src_y = src_tile / chip_info->width;
     }
-    return {std::min(src_x, dst_x), std::min(src_y, dst_y), std::max(src_x, dst_x), std::max(src_y, dst_y)};
+    return {x0, y0, x1, y1};
 }
 
 delay_t Arch::getBoundingBoxCost(WireId src, WireId dst, int distance) const
