c2cc5acf057f92af8ed4a0e227cfdf9f11ef375a
David Shah <dave@ds0.me>
2020-02-13 15:33:33 +0000

    Use new partfile path in zynq7 example
    Signed-off-by: David Shah <dave@ds0.me>

--- xilinx/examples/artyz7-20/blinky.sh
+++ xilinx/examples/artyz7-20/blinky.sh
@@ -4,4 +4,4 @@ yosys -p "synth_xilinx -flatten -abc9 -arch xc7 -top top; write_json blinky.json
 ../../../nextpnr-xilinx --chipdb ../../xc7z020.bin --xdc artyz7.xdc --json blinky.json --write blinky_routed.json --fasm blinky.fasm
 source "${XRAY_DIR}/utils/environment.sh"
 ${XRAY_UTILS_DIR}/fasm2frames.py --part xc7z020clg400-1 --db-root ${XRAY_UTILS_DIR}/../database/zynq7 blinky.fasm > blinky.frames
-${XRAY_TOOLS_DIR}/xc7frames2bit --part_file ${XRAY_UTILS_DIR}/../database/zynq7/xc7z020clg400-1.yaml --part_name xc7z020clg400-1 --frm_file blinky.frames --output_file blinky.bit
+${XRAY_TOOLS_DIR}/xc7frames2bit --part_file ${XRAY_UTILS_DIR}/../database/zynq7/xc7z020clg400-1/part.yaml --part_name xc7z020clg400-1 --frm_file blinky.frames --output_file blinky.bit
