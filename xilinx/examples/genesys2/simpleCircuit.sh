d211422a5a57a1b9454af3cc791695426e5693e7
asanaullah <58180328+asanaullah@users.noreply.github.com>
2020-02-19 11:36:36 -0500

    Update simpleCircuit.sh

--- xilinx/examples/genesys2/simpleCircuit.sh
+++ xilinx/examples/genesys2/simpleCircuit.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -ex
-yosys -p "synth_xilinx -flatten -arch xcup -nobram -nowidelut -top top; write_json simpleCircuit.json" simpleCircuit.v
+yosys -p "synth_xilinx -flatten -arch xc7 -nobram -nowidelut -top top; write_json simpleCircuit.json" simpleCircuit.v
 ../../../nextpnr-xilinx --chipdb ../../xc7k325t.bin --json simpleCircuit.json --xdc Genesys2.xdc --write simpleCircuit_routed.json
 python kintex7patch.py simpleCircuit_routed.json
 java -jar ../../../rapidwright_json2dcp.jar xc7k325tffg900-2 simpleCircuit_routed.json simpleCircuit.dcp
