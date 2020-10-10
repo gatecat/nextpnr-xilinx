d72f0a5e2ef92d9ab929575d81e70c06633634ae
Ignacio Lesser <nachoyo89@gmail.com>
2020-10-10 15:16:42 +0200

    Updated source files for yosys

--- xilinx/examples/ultra96/blinky.sh
+++ xilinx/examples/ultra96/blinky.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -ex
-yosys -p "synth_xilinx -flatten -arch xcup -nobram -top top; write_json blinky.json" ../attosoc/attosoc.v blinky.v
+yosys -p "synth_xilinx -flatten -arch xcup -nobram -top top; write_json blinky.json" zynq_ultra_ps_e_0.v top.v
 ../../../nextpnr-xilinx --chipdb ../../xczu3eg.bin --json blinky.json --xdc ultra96.xdc --write blinky_routed.json
 java -jar ../../../rapidwright_json2dcp.jar xczu3eg-sbva484-1-e blinky_routed.json blinky.dcp
 vivado -mode batch -nojournal -nolog  -source blinky.tcl
