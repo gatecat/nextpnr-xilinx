cb9c8e5cc78486ebc4cadc9ff99c4e64ff854d57
Ignacio Lesser <nachoyo89@gmail.com>
2020-10-10 15:15:20 +0200

    Added zynq ultra PS to RTL

---
+++ xilinx/examples/ultra96/top.v
@@ -0,0 +1,37 @@
+`timescale 1 ps / 1 ps
+
+module top (output led);
+
+	wire zynq_ultra_ps_e_0_pl_clk0;
+	reg [31:0] ctr;
+
+	always @(posedge zynq_ultra_ps_e_0_pl_clk0) {led, ctr} <= ctr + 32'd1;
+	zynq_ultra_ps_e_0 u_zynq_ultra_ps_e_0 (
+		.maxihpm0_fpd_aclk(zynq_ultra_ps_e_0_pl_clk0),
+		.maxihpm1_fpd_aclk(zynq_ultra_ps_e_0_pl_clk0),
+		.pl_clk0(zynq_ultra_ps_e_0_pl_clk0),
+		.maxigp0_arready(1'b0),
+		.maxigp0_awready(1'b0),
+		.maxigp0_bid({1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0}),
+		.maxigp0_bresp({1'b0,1'b0}),
+		.maxigp0_bvalid(1'b0),
+		.maxigp0_rdata({1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0}),
+		.maxigp0_rid({1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0}),
+		.maxigp0_rlast(1'b0),
+		.maxigp0_rresp({1'b0,1'b0}),
+		.maxigp0_rvalid(1'b0),
+		.maxigp0_wready(1'b0),
+		.maxigp1_arready(1'b0),
+		.maxigp1_awready(1'b0),
+		.maxigp1_bid({1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0}),
+		.maxigp1_bresp({1'b0,1'b0}),
+		.maxigp1_bvalid(1'b0),
+		.maxigp1_rdata({1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0}),
+		.maxigp1_rid({1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0}),
+		.maxigp1_rlast(1'b0),
+		.maxigp1_rresp({1'b0,1'b0}),
+		.maxigp1_rvalid(1'b0),
+		.maxigp1_wready(1'b0),
+		.pl_ps_irq0(1'b0)
+	);
+endmodule
---
+++ xilinx/examples/ultra96/zynq_ultra_ps_e_0.v
@@ -0,0 +1,32 @@
+`timescale 1 ps / 1 ps
+
+(* blackbox *)
+module zynq_ultra_ps_e_0 (
+	output pl_clk0,
+	input maxihpm0_fpd_aclk,
+	input maxihpm1_fpd_aclk,
+	input maxigp0_arready,
+	input maxigp0_awready,
+	input maxigp0_bid,
+	input maxigp0_bresp,
+	input maxigp0_bvalid,
+	input maxigp0_rdata,
+	input maxigp0_rid,
+	input maxigp0_rlast,
+	input maxigp0_rresp,
+	input maxigp0_rvalid,
+	input maxigp0_wready,
+	input maxigp1_arready,
+	input maxigp1_awready,
+	input maxigp1_bid,
+	input maxigp1_bresp,
+	input maxigp1_bvalid,
+	input maxigp1_rdata,
+	input maxigp1_rid,
+	input maxigp1_rlast,
+	input maxigp1_rresp,
+	input maxigp1_rvalid,
+	input maxigp1_wready,
+	input pl_ps_irq0
+);
+endmodule
