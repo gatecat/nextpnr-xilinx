f3f1fe668ce136fa04d4571b68db9264c3a044c8
Ignacio Lesser <nachoyo89@gmail.com>
2021-02-07 21:37:07 +0100

    Used xilinx primitives for the example

--- xilinx/examples/ultra96/top.v
+++ xilinx/examples/ultra96/top.v
@@ -1,37 +1,18 @@
+`default_nettype none
 `timescale 1 ps / 1 ps
 
 module top (output led);
 
-	wire zynq_ultra_ps_e_0_pl_clk0;
+	wire clk, led;
 	reg [31:0] ctr;
 
-	always @(posedge zynq_ultra_ps_e_0_pl_clk0) {led, ctr} <= ctr + 32'd1;
-	zynq_ultra_ps_e_0 u_zynq_ultra_ps_e_0 (
-		.maxihpm0_fpd_aclk(zynq_ultra_ps_e_0_pl_clk0),
-		.maxihpm1_fpd_aclk(zynq_ultra_ps_e_0_pl_clk0),
-		.pl_clk0(zynq_ultra_ps_e_0_pl_clk0),
-		.maxigp0_arready(1'b0),
-		.maxigp0_awready(1'b0),
-		.maxigp0_bid({1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0}),
-		.maxigp0_bresp({1'b0,1'b0}),
-		.maxigp0_bvalid(1'b0),
-		.maxigp0_rdata({1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0}),
-		.maxigp0_rid({1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0}),
-		.maxigp0_rlast(1'b0),
-		.maxigp0_rresp({1'b0,1'b0}),
-		.maxigp0_rvalid(1'b0),
-		.maxigp0_wready(1'b0),
-		.maxigp1_arready(1'b0),
-		.maxigp1_awready(1'b0),
-		.maxigp1_bid({1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0}),
-		.maxigp1_bresp({1'b0,1'b0}),
-		.maxigp1_bvalid(1'b0),
-		.maxigp1_rdata({1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0}),
-		.maxigp1_rid({1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0}),
-		.maxigp1_rlast(1'b0),
-		.maxigp1_rresp({1'b0,1'b0}),
-		.maxigp1_rvalid(1'b0),
-		.maxigp1_wready(1'b0),
-		.pl_ps_irq0(1'b0)
+	always @(posedge clk) {led, ctr} <= ctr + 32'd1;
+	wire [3:0] plclk;
+	PS8 ps_i(
+		.PLCLK(plclk)
+	);
+	BUFG_PS bfg_i(
+		.I(plclk[0]),
+		.O(clk)
 	);
 endmodule
